e70956803b74f6b60b4cf8bd8946f4075eeb1c04
morings <1620201780@qq.com>
2019-10-18 14:59:10 +0800

    配置修改

--- template/.postcssrc.js
+++ template/.postcssrc.js
@@ -7,7 +7,7 @@ module.exports = {
     // to edit target browsers: use "browserslist" field in package.json
     "autoprefixer": {},
     "postcss-px2rem-exclude": {  // 添加的代码
-      remUnit: 1,
+      remUnit: 100,
       exclude: /node_modules|folder_name/i // 忽略node_modules目录下的文件
     }
   }

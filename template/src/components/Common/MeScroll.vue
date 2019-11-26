d17b6d00f08184f5a4f0eaa5bcabeba7ea66235e
morings <1620201780@qq.com>
2019-11-26 14:59:55 +0800

    代码提交

---
+++ template/src/components/Common/MeScroll.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="mescroll" ref="MeScroll">
+    <div>
+
+    </div>
+  </div>
+</template>
+<script>
+import MeScroll from 'mescroll.js'
+import 'mescroll.js/mescroll.min.css'
+export default {
+  
+}
+</script>

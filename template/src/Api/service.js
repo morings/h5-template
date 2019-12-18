34edcc205c9e3a6fda90df565e3c479172e9598a
morings <1620201780@qq.com>
2019-12-18 14:47:13 +0800

    template更新

--- template/src/Api/service.js
+++ template/src/Api/service.js
@@ -16,29 +16,15 @@ const errorInfo = {
 }
 var MaskLoad = null;
 service.interceptors.request.use(config => {
-  MaskLoad = Loading.service({
-    lock: true,
-    text: '数据请求中',
-    spinner: 'el-icon-loading',
-    background: 'rgba(0, 0, 0, 0.7)'
-  });
   return config;
 }, error => {  //请求错误处理
   return Promise.reject(error)
 });
 service.interceptors.response.use(
   response => {  //成功请求到数据
-      if(MaskLoad){
-        MaskLoad.close();
-        MaskLoad = null;
-      }
       return response.data;
   },
   error => {  //响应错误处理
-    if(MaskLoad){
-      MaskLoad.close();
-      MaskLoad = null;
-    }
     showerror(error.response.data);
     return Promise.reject(error)
   }
@@ -47,27 +33,7 @@ function showerror(data){
   msgerror(errorInfo[data.error_code])
   
 };
-function deleteEmptyProperty(object){
-  for (var i in object) {
-    var value = object[i];
-    if (typeof value === 'object') {
-      if (Array.isArray(value)) {
-        if (value.length == 0) {
-          delete object[i];
-          continue;
-        }
-      }
-      deleteEmptyProperty(value);
-      if (isEmpty(value)) {
-        delete object[i];
-      }
-    } else {
-      if (value === '' || value === null || value === undefined) {
-        delete object[i];
-      }
-    }
-  }
-}
+
 
 function isEmpty(object) {
   for (var name in object) {
